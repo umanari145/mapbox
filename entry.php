70955833993b01fb94c2e2606da73940c4bdda99
matsumoto norio <umanari145@gmail.com>
2021-12-19 11:01:13 +0900

    .envの読み込みを除去

--- entry.php
+++ entry.php
@@ -8,6 +8,3 @@ require_once __DIR__ .'/vendor/autoload.php';
 //ライブラリの読み込み
 require_once __DIR__ .'/libs/Database.php';
 require_once __DIR__ .'/libs/LogUtil.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
